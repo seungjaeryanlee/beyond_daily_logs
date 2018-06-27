aff18d3e9c3919e3f9294f2a8c6b1f13fe00dc76
Seung Jae (Ryan) Lee <seungjaeryanlee@gmail.com>
2018-06-27 14:39:54 +0900

    Organized test_app.py
     * Specified test in function name
     * Divided tests into smaller units
     * Added few TODOs

--- test_app.py
+++ test_app.py
@@ -4,6 +4,8 @@ import unittest
 import os
 from utils import get_logs_files
 
+
+# TODO Rename 'BasicTestCode' class
 class BasicTestCode(unittest.TestCase):
 
     def text_index(self):
@@ -13,26 +15,26 @@ class BasicTestCode(unittest.TestCase):
         self.assertEqual(resp.status_code, 200)
         self.assertEqual(resp.data, b'Beyond daily logs')
 
-    def test_database(self):
+    def test_database_existance(self):
         """Check existance of database"""
         db_path = os.path.join(os.getcwd(), app.config['DB_DIR'])
-        tester = os.path.isdir(db_path)
-        self.assertTrue(tester)
-        tester = os.path.isfile(os.path.join(db_path, app.config['LOGS_DB'], '2017.pkl'))
-        self.assertTrue(tester)
-        tester = os.path.isfile(os.path.join(db_path, app.config['LOGS_DB'], '2018.pkl'))
-        self.assertTrue(tester)
-        tester = os.path.isfile(os.path.join(db_path, app.config['USERS_DB']))
-        self.assertTrue(tester)
+        self.assertTrue(os.path.isdir(db_path))
+        self.assertTrue(os.path.isfile(os.path.join(db_path, app.config['LOGS_DB'], '2017.pkl')))
+        self.assertTrue(os.path.isfile(os.path.join(db_path, app.config['LOGS_DB'], '2018.pkl')))
+        self.assertTrue(os.path.isfile(os.path.join(db_path, app.config['USERS_DB'])))
 
-    def test_exported_data(self):
-        """Check existance and file extension of exported database"""
+    def test_exported_database_existance(self):
+        """Check existance of exported database"""
         db_path = os.path.join(os.getcwd(), app.config['EXPORTED_DB_DIR'])
-        tester = os.path.isdir(db_path)
-        self.assertTrue(tester)
+        self.assertTrue(os.path.isdir(db_path))
+
+    def test_exported_database_file_extension(self):
+        """Check file extension of exported database"""
+        db_path = os.path.join(os.getcwd(), app.config['EXPORTED_DB_DIR'])
 
         file_names = []
         logs_files = get_logs_files(db_path)
+        # TODO Why iterate both logs_files.values() and file_names()?
         for files in logs_files.values():
             file_names += files
         for name in file_names:
